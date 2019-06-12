627ad450e69aa3b8b485bb867517a815137e7943
Edson Prestes <edson_prestes@epam.com>
2019-06-12 13:23:30 +0200

    Challenge of Geometric Figures

--- java-logic/src/main/java/com/epam/mentoring/java/challange/FiguresLogic.java
+++ java-logic/src/main/java/com/epam/mentoring/java/challange/FiguresLogic.java
@@ -1,5 +1,13 @@
 package com.epam.mentoring.java.challange;
 
+/**
+ * Challenge of Geometric Figures
+ * <p>
+ * Issue #1
+ *
+ * @author <a href="mailto:edson_prestes@epam.com">Edson Prestes</a>
+ * @version 1.0
+ */
 public class FiguresLogic {
 
     public static void main(String[] args) {
