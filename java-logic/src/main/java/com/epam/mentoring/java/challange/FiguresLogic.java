4ee3d46ee3d2af13a8c55140cadab3d0e8323b01
Marcin_Chodkowski <marcin_chodkowski@epam.com>
2019-06-26 12:19:45 +0200

    test commit

--- java-logic/src/main/java/com/epam/mentoring/java/challange/FiguresLogic.java
+++ java-logic/src/main/java/com/epam/mentoring/java/challange/FiguresLogic.java
@@ -11,6 +11,6 @@ package com.epam.mentoring.java.challange;
 public class FiguresLogic {
 
     public static void main(String[] args) {
-        // TODO Coding your solution here
+        // TODO Coding your solution heres
     }
 }
