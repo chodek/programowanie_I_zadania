0549a008648d96aefa82e4199ad24d42cc140677
chodekm <chodekm@gmail.com>
2021-10-17 12:40:17 +0200

    Adding maven

--- programowanie_I/src/main/zadania/sredniowiecze/Main.java
+++ programowanie_I/src/main/zadania/sredniowiecze/Main.java
@@ -7,13 +7,13 @@ public class Main {
         Soldier soldier = new Soldier("Gorn");
         Townsman townsman = new Townsman("Thorben");
 
-        Town town = new Town(new Citizen[]{king,peasant,soldier,townsman}); // Tworze nowe miasto, przekazaując do konstruktora tworzona tablice Citizenow, skladajaca sie z krola, chlopa, zolnierza i mieszczanina.
+        Town town = new Town(new Citizen[]{king, peasant, soldier, townsman}); // Tworze nowe miasto, przekazaując do konstruktora tworzona tablice Citizenow, skladajaca sie z krola, chlopa, zolnierza i mieszczanina.
         System.out.println("Ilosc osob mogaca glosowac: " + town.howManyCanVote());
         town.whoCanVote();
 
-        Citizen []peopleWhoCanVote = town.whoCanVote2();
+        Citizen[] peopleWhoCanVote = town.whoCanVote2();
         System.out.println("W tablicy z ludzmi mogacymi glosowac sa:");
-        for(Citizen citizen : peopleWhoCanVote){
+        for (Citizen citizen : peopleWhoCanVote) {
             System.out.println(citizen.getName());
         }
     }
